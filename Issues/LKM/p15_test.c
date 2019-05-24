eddf3eea735ec9c06f7eaa46e15984d0adc49ce3
Zoran Stojsavljevic <zoran.stojsavljevic@gmail.com>
2019-05-24 13:28:27 +0200

    Update p15_test.c

--- Issues/LKM/p15_test.c
+++ Issues/LKM/p15_test.c
@@ -6,13 +6,13 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
 
-MODULE_LICENSE("GPL");                                ///< The license type -- this affects available functionality
-MODULE_AUTHOR("Zoran Stojsavljevic");                 ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("A simple armv7 A8 test driver");  ///< The description -- see modinfo
-MODULE_VERSION("0.01");                               ///< A version number to inform users
+MODULE_LICENSE("GPL");					///< The license type -- this affects available functionality
+MODULE_AUTHOR("Zoran Stojsavljevic");			///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("A simple armv7 A8 test driver");	///< The description -- see modinfo
+MODULE_VERSION("0.01");					///< A version number to inform users
 
 static int __init p15_test_init(void) {
-        unsigned int reg_value = 0;
+	unsigned int reg_value = 0;
 
 	printk(KERN_INFO "p15_Test init\n");
 	asm volatile("mrc p15, 0, %0, c0, c0, 0" : "=r"(reg_value) );
